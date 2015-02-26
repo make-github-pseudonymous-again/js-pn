b63610be5b947522b4a7b15d2f21e692094743aa
aureooms <aurelien.ooms@gmail.com>
2015-02-26 23:16:41 +0100

    v0.1.0

--- js/dist/pn.js
+++ js/dist/pn.js
@@ -3,6 +3,54 @@
 	'use strict';
 
 
-/* js/src/dummy.js */
+/* js/src/pull.js */
+
+var pull = function ( compile , isop , token , stack , count ) {
+
+	var state ;
+
+	stack.push( token ) ;
+
+	if ( isop( token ) ) {
+
+		count.push( 0 ) ;
+		return false ;
+
+	}
+
+	while ( true ) {
+
+		state = count.pop( ) + 1 ;
+		count.push( state ) ;
+
+		if ( state < 2 ) return false ;
+
+		b  = stack.pop( ) ;
+		a  = stack.pop( ) ;
+		op = stack.pop( ) ;
+
+		stack.push( compile( op , a , b ) ) ;
+		count.pop( ) ;
+
+		if ( count.empty( ) ) return true ;
+
+	}
+
+} ;
+
+
+/* js/src/push.js */
+
+var push = function ( compile , isop , tokens , stack , count ) {
+
+	var token ;
+
+	do {
+
+		token = tokens.next( ) ;
+
+	} while ( pull( compile , isop , token , stack , count ) ) ;
+
+} ;
 
 })(typeof exports === 'undefined' ? this['pn'] = {} : exports);
